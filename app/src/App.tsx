2b5835bdceb037208105702509e19c81ea55023a
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2025-02-24 07:00:22 +0800

    feat: disable login page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/home";
 import WrongPage from "./pages/404";
-import LoginPage from "./pages/login";
+// import LoginPage from "./pages/login";
 import AgentsPage from "./pages/agents";
 
 function App() {
   return (
     <Routes>
-      <Route path="/watchers" element={<HomePage />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/agents" element={<AgentsPage />} />
-      <Route path="/" element={<LoginPage />} />
+      {/* <Route path="/" element={<LoginPage />} /> */}
       <Route path="*" element={<WrongPage />} />
     </Routes>
   );
