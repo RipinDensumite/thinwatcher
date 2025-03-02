8fa47ff525df1625f9e02fa540f02e667839b5eb
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2025-03-02 17:39:54 +0800

    feat: add individual page title

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,13 +4,49 @@ import WrongPage from "./pages/404";
 // import LoginPage from "./pages/login";
 import AgentsPage from "./pages/agents";
 
+function Title({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <title>{title}</title>
+      {children}
+    </>
+  );
+}
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/agents" element={<AgentsPage />} />
+      <Route
+        path="/"
+        element={
+          <Title title="Watchers">
+            <HomePage />
+          </Title>
+        }
+      />
+      <Route
+        path="/agents"
+        element={
+          <Title title="Agents">
+            <AgentsPage />
+          </Title>
+        }
+      />
       {/* <Route path="/" element={<LoginPage />} /> */}
-      <Route path="*" element={<WrongPage />} />
+      <Route
+        path="*"
+        element={
+          <Title title="404 - Page Not Found">
+            <WrongPage />
+          </Title>
+        }
+      />
     </Routes>
   );
 }
