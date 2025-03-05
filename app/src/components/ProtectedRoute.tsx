3ce67b20c2498c6c79aef5ad8e1d58d0cbca96e2
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2025-03-05 09:40:54 +0800

    feat: change loading spinner

--- app/src/components/ProtectedRoute.tsx
+++ app/src/components/ProtectedRoute.tsx
@@ -18,7 +18,7 @@ const ProtectedRoute = ({
   if (loading) {
     return (
       <div className="min-h-screen flex items-center justify-center">
-        <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-blue-600"></div>
+        <span className="loading loading-spinner loading-xl"></span>
       </div>
     );
   }
