37274d05b1e6a2ed643922a31d2697e4f91460d7
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2025-03-05 15:00:53 +0800

    feat: replace status symbol with dot symbol

--- app/src/pages/home/index.tsx
+++ app/src/pages/home/index.tsx
@@ -216,30 +216,18 @@ export default function HomePage() {
                     <h2 className="text-xl font-semibold">{client.clientId}</h2>
                     {client.status.isOnline ? (
                       <span className="select-none inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                          className="h-3 w-3"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
+                        <div className="inline-grid *:[grid-area:1/1]">
+                          <div className="status status-success animate-ping"></div>
+                          <div className="status status-success"></div>
+                        </div>
                         Healthy
                       </span>
                     ) : (
                       <span className="select-none inline-flex items-center gap-1 rounded-full bg-red-50 px-2 py-1 text-xs font-semibold text-red-600">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                          className="h-3 w-3"
-                        >
-                          <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-                        </svg>
+                        <div className="inline-grid *:[grid-area:1/1]">
+                          <div className="status status-error animate-ping"></div>
+                          <div className="status status-error"></div>
+                        </div>
                         Unhealthy
                       </span>
                     )}
@@ -411,41 +399,31 @@ export default function HomePage() {
                       <td className="px-6 py-4">
                         {client.status.isOnline ? (
                           <span className="select-none inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                              className="h-3 w-3"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                clip-rule="evenodd"
-                              />
-                            </svg>
+                            <div className="inline-grid *:[grid-area:1/1]">
+                              <div className="status status-success animate-ping"></div>
+                              <div className="status status-success"></div>
+                            </div>
                             Healthy
                           </span>
                         ) : (
                           <span className="select-none inline-flex items-center gap-1 rounded-full bg-red-50 px-2 py-1 text-xs font-semibold text-red-600">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                              className="h-3 w-3"
-                            >
-                              <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-                            </svg>
+                            <div className="inline-grid *:[grid-area:1/1]">
+                              <div className="status status-error animate-ping"></div>
+                              <div className="status status-error"></div>
+                            </div>
                             Unhealthy
                           </span>
                         )}
                       </td>
                       <td className="flex justify-end gap-4 px-6 py-4 font-medium">
-                        {user?.role === "admin" && <button
-                          onClick={() => deleteClient(client.clientId)}
-                          className="cursor-pointer"
-                        >
-                          <Trash2 size={18} />
-                        </button>}
+                        {user?.role === "admin" && (
+                          <button
+                            onClick={() => deleteClient(client.clientId)}
+                            className="cursor-pointer"
+                          >
+                            <Trash2 size={18} />
+                          </button>
+                        )}
                       </td>
                     </tr>
 
