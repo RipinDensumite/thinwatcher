fca8e3ff71ab60cc602ebdabe152762770642efc
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2025-03-06 10:56:08 +0800

    feat: decrease CircleUserRound strokeWidth to 1.2

--- app/src/layout/layout.tsx
+++ app/src/layout/layout.tsx
@@ -118,7 +118,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 <div className="flex flex-row items-center gap-2">
                   <div className="avatar">
                     <div className="w-8">
-                      <CircleUserRound size={32} color="black" />
+                      <CircleUserRound size={32} color="black" strokeWidth={1.2}/>
                       {/* <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" /> */}
                     </div>
                   </div>
