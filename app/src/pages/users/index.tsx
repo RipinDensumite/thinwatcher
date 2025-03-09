b8f0f1a4741c6b1c58a57b7ee32263db61ca97ef
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2025-03-10 04:26:46 +0800

    feat: Make create input empty after cancel

--- app/src/pages/users/index.tsx
+++ app/src/pages/users/index.tsx
@@ -388,31 +388,6 @@ function ManageUsersPage() {
               </span>
 
               <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                <div className="absolute top-0 right-0 pt-4 pr-4">
-                  <button
-                    type="button"
-                    className="cursor-pointer bg-white rounded-md text-gray-400 hover:text-gray-500 focus:bg-gray-300"
-                    onClick={() => setIsCreateModalOpen(false)}
-                  >
-                    <span className="sr-only">Close</span>
-                    <svg
-                      className="h-6 w-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </button>
-                </div>
-
                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-slate-100 sm:mx-0 sm:h-10 sm:w-10">
@@ -542,7 +517,14 @@ function ManageUsersPage() {
                             <button
                               type="button"
                               className="cursor-pointer mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 sm:mt-0 sm:w-auto sm:text-sm"
-                              onClick={() => setIsCreateModalOpen(false)}
+                              onClick={() => {
+                                setIsCreateModalOpen(false);
+                                setNewUser({
+                                  username: "",
+                                  email: "",
+                                  password: "",
+                                });
+                              }}
                             >
                               Cancel
                             </button>
