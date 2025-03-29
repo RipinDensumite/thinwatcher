e1e25b631bf0b4938c0d63eea7bcb94104a4d323
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2025-03-30 00:37:46 +0800

    feat: remove env backend

--- app/src/utils/appconfig.ts
+++ app/src/utils/appconfig.ts
@@ -1,3 +1,4 @@
 export const APP_CONFIG = {
-  BACKEND_API_URL: import.meta.env.VITE_BACKEND_API_URL as string,
+  // BACKEND_API_URL: import.meta.env.VITE_BACKEND_API_URL as string,
+  BACKEND_API_URL: "",
 };
